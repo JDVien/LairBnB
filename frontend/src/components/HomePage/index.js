83554d8951895e6039cb4963c3a14d2783ebec81
jasonv <jdvn2517@gmail.com>
2022-04-03 19:31:37 -0700

    cards, styling

--- frontend/src/components/HomePage/index.js
+++ frontend/src/components/HomePage/index.js
@@ -1,13 +1,46 @@
 import React from 'react';
+import Card from '../Card/Card';
 import './HomePage.css';
 function HomePage() {
   return (
+    <div>
+    <div className="landing_image_body" >
     <div className='landing_image_bg'>
       <div className='brave_booking_container'>
       <h2>Let your morbid curiosity do the booking</h2>
       <button className='random_spot_brave_bttn'>I'm Brave</button>
       <a href='/'>
       </a>
+    </div>
+    </div>
+    </div>
+    <div className='home_card_body'>
+      <Card src='https://seattlerefined.com/resources/media/05b9d5de-4f6f-4f13-aa4a-67e709981102-large16x9_a.JPG?1575572677165'
+            title="homes"
+            description="Private homes, with emphasis on private"
+      />
+      <Card src='https://seattlerefined.com/resources/media/05b9d5de-4f6f-4f13-aa4a-67e709981102-large16x9_a.JPG?1575572677165'
+            title="homes"
+            description="Private homes, with emphasis on private"
+      />
+      <Card src='https://seattlerefined.com/resources/media/05b9d5de-4f6f-4f13-aa4a-67e709981102-large16x9_a.JPG?1575572677165'
+            title="homes"
+            description="Private homes, with emphasis on private"
+      />
+    </div>
+    <div className='home_card_body'>
+      <Card src='https://seattlerefined.com/resources/media/05b9d5de-4f6f-4f13-aa4a-67e709981102-large16x9_a.JPG?1575572677165'
+            title="homes"
+            description="Private homes, with emphasis on private"
+      />
+      <Card src='https://seattlerefined.com/resources/media/05b9d5de-4f6f-4f13-aa4a-67e709981102-large16x9_a.JPG?1575572677165'
+            title="homes"
+            description="Private homes, with emphasis on private"
+      />
+      <Card src='https://seattlerefined.com/resources/media/05b9d5de-4f6f-4f13-aa4a-67e709981102-large16x9_a.JPG?1575572677165'
+            title="homes"
+            description="Private homes, with emphasis on private"
+      />
     </div>
     </div>
   )
--- frontend/src/components/Search/index.js
+++ frontend/src/components/Search/index.js
@@ -1,10 +1,31 @@
-// import React from 'react';
+import React from 'react';
+import { useState } from 'react';
+import './Search.css';
+import { DateRangePicker } from 'react-date-range';
 
-// import './Search.css';
 
-// // function Search() {
-// //   return (
-// //   )
-// // }
+// search for an available date
+function Search() {
+  const [start, setStart] = useState(new Date());
+  const [end, setEnd] = useState(new Date());
 
-// export default Search;
+  const DATE_RANGE = {
+    start: start,
+    end: end,
+    key: "selection"
+  }
+
+  function handleSelect(ranges) {
+    setStart(ranges.selection.start);
+    setEnd(ranges.selection.end);
+  }
+
+  return (
+    <div className='search'>
+      <DateRangePicker ranges={[DATE_RANGE]} onChange={handleSelect} />
+      }
+    </div>
+  )
+}
+
+export default Search;
